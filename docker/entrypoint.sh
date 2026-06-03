1a9db3e8bdc462425eac1de1890ac42376cbd4f3
Matheus Braga de Carvalho <matheusbc1234@gmail.com>
2026-06-03 16:39:41 -0300

    Update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "[entrypoint] Aplicando migrations..."
 flask db upgrade
 
+echo "[entrypoint] Populando dados base (cargos, áreas, prioridades)..."
+flask seed
+
 echo "[entrypoint] Iniciando gunicorn..."
 exec gunicorn \
     --workers "${GUNICORN_WORKERS:-3}" \
